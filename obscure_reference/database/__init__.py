b01e7bc86cf3665fb4a38d375a7a37e1026cda94
pwforbes <p.m.forbes@gmail.com>
2012-02-17 00:39:22 -0600

    created obscure reference package

---
+++ obscure_reference/database/__init__.py
@@ -0,0 +1,11 @@
+##
+# This file is used for and initialization
+# of the database package.
+#
+# Author: Paul Forbes
+#
+# Date: February 16, 2012
+#
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
