216dfaa39ea34a0c01d1263a8d644a8bd278b295
Jason Kamphaugh <jkamph@juno.com>
2012-02-16 22:35:29 -0600

    Upload string definitions

---
+++ common/string_definitions.py
@@ -0,0 +1,14 @@
+##
+# This file is the common definitions to be used in many files.
+#
+# Author: Jason Kamphaugh
+#
+# Date: February 16, 2012
+#
+
+player_table_name = "Player"
+
+default_database_name = "Obscure Reference Database"
+
+#end string_definitions
+
